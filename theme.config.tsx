32040698ac318cce9a31d41b56f99474ac8ccc63
Aradhya Mehta <aradhyamehta1@gmail.com>
2023-02-15 16:29:17 +0530

    Removed Dsicord Icon. Needs more edits

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,16 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>System Design</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+  ),
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
